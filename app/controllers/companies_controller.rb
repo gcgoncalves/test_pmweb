372b8290c25f9e9fcf46f859b6986bfedafd2a8d
Gabriel Costa Gonçalves <gcgoncalves88@gmail.com>
2014-01-18 16:41:17 -0200

    adicionada a classe Company e alguns de seus métodos e views

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,40 @@
 class CompaniesController < ApplicationController
-    def new
+    def index
+    	@companies = Company.all
     end
+
+    def new
+		@company = Company.new
+    end
+
+	def edit
+		@company = Company.find(params[:id])
+	end
+
+	def destroy
+		@company = Company.find(params[:id])
+		@company.destroy
+
+		redirect_to companies_path
+	end    
+
+    def create
+    	@company = Company.new(company_params)
+  		
+  		if @company.save
+  			redirect_to @company
+ 		else
+    		render 'new'
+    	end
+    end
+
+	def company_params
+		params.require(:post).permit(:name, :legal_name, :zip_code, :address, :phone, :company_type)
+	end
+
+	def show
+		@company = Company.find(params[:id])
+	end
+
+	private :company_params
 end
